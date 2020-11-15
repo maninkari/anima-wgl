5c3b15b81325ac42b871bf01d2d9a73e4fb731b3
roberto.torres <ts.roberto@gmail.com>
2020-11-15 00:50:25 +0000

    inicio

---
+++ pages/anima.vue
@@ -0,0 +1,34 @@
+<template>
+  <main>
+    <h1>Anima</h1>
+    <canvas id="anima"></canvas>
+  </main>
+</template>
+
+<script>
+import * as THREE from 'three'
+
+const WIDTH = 640.0
+const HEIGHT = 480.0
+
+export default {
+  name: 'Anima',
+  data() {
+    return {
+      renderer: {},
+    }
+  },
+  mounted() {
+    this.startWgl()
+  },
+  methods: {
+    startWgl() {
+      this.renderer = new THREE.WebGLRenderer({
+        canvas: document.getElementById('anima'),
+        antialias: false,
+      })
+      this.renderer.setSize(WIDTH, HEIGHT)
+    },
+  },
+}
+</script>
