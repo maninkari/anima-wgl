494d20166b9fe51a1079fbd7ec4d8864ce5eea15
roberto.torres <ts.roberto@gmail.com>
2020-11-21 20:24:55 +0000

    camera

--- pages/anima.vue
+++ pages/anima.vue
@@ -23,11 +23,33 @@ export default {
   },
   methods: {
     startWgl() {
+      const scene = new THREE.Scene()
+      const camera = new THREE.PerspectiveCamera(60, 640.0 / 480.0, 1, 1000)
+
+      camera.position.z = 640.0
+      camera.zoom = 1.0
+
+      const geometry = new THREE.PlaneGeometry(480.0, 360.0)
+      const material = new THREE.MeshBasicMaterial({
+        color: '#ff0000',
+        side: THREE.DoubleSide,
+      })
+
+      const plane = new THREE.Mesh(geometry, material)
+      scene.add(plane)
+
       this.renderer = new THREE.WebGLRenderer({
         canvas: document.getElementById('anima'),
         antialias: false,
       })
       this.renderer.setSize(WIDTH, HEIGHT)
+
+      const animate = () => {
+        requestAnimationFrame(animate)
+        this.renderer.render(scene, camera)
+      }
+
+      animate()
     },
   },
 }
